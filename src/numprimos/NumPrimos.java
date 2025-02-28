97e0966a6581984c70af9c5fcf83c4a75a7d9beb
Victor <victorfernandez574@gmail.com>
2025-02-28 18:49:56 +0100

    que pasa

--- src/numprimos/NumPrimos.java
+++ src/numprimos/NumPrimos.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package numprimos;
-
+//Prueba commit Hola como estas
 import java.util.Scanner;
 
 /**
